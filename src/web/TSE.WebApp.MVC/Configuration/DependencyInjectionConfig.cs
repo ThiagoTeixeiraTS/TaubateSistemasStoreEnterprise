d91f4b5a60e42c3473c17f1679d2aa1f09ed5397
Thiago Araujo <arsaoinbdevops@outlook.com>
2022-09-14 20:02:17 -0300

    Commit - Criando a tela de autenticação e consumindo a API com o Identity

---
+++ src/web/TSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using TSE.WebApp.MVC.Extensions;
+using TSE.WebApp.MVC.Services;
+
+namespace TSE.WebApp.MVC.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUser, AspNetUser>();
+        }
+    }
+}
---
+++ src/web/TSE.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using TSE.WebApp.MVC.Models;
+
+namespace TSE.WebApp.MVC.Services
+{
+    public interface IAutenticacaoService
+    {
+        Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin);
+
+        Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro);
+    }
+}
